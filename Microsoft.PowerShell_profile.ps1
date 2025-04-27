e53fa9b6e9009ff778e35c143b8bc64cc0c8d66b
Ryan Luong <rluong@tae.com>
2025-04-26 19:28:57 -0700

    Sync

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,62 @@
+$versionMinimum = [Version]'7.1.999'
+
+if (($host.Name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $versionMinimum))
+{
+	Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+}
+else
+{
+	Set-PSReadLineOption -PredictionSource History
+}
+
+New-Alias Goto Set-Location
+
+$ProfileDirectory = Split-Path -Path (gci $profile)
+gci "$ProfileDirectory\my_custom_aliases.ps1" | foreach-object { . $_ }
+
+Set-PSReadLineKeyHandler -Chord Tab     -Function Complete
+Set-PSReadLineKeyHandler -Key UpArrow   -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-PSReadLineOption -EditMode Windows
+
+Set-PSReadLineKeyHandler -Key Alt+w `
+-BriefDescription SaveInHistory `
+-ScriptBlock {
+	param($key, $arg)
+
+	$line = $null
+	$cursor = $null
+	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+	[Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+	[Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+}
+
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+Delete -ScriptBlock {
+
+	$in = ''
+
+	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $in, [ref] $null)
+
+	if ([string]::IsNullOrWhiteSpace($in)) {
+		return
+	}
+
+	$historyPath = (Get-PSReadLineOption).HistorySavePath
+
+	[Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+	$content = [System.IO.File]::ReadAllLines($historyPath)
+	Clear-Content $historyPath
+
+	foreach ($line in $content) {
+		if ($line.StartsWith($in, [System.StringComparison]::InvariantCultureIgnoreCase)) {
+			continue
+		}
+
+		[Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+	}
+	[Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
+}
---
+++ my_custom_aliases.ps1
@@ -0,0 +1,3 @@
+# New-Alias cdmachinestatedatabase Set-Location .\Documents\Projects\Git\machine_state_database
+
+function cdmachinestatedatabase { set-location "$PROFILE\Documents\Projects\Git\machine_state_database" }
