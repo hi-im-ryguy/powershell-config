88c5f9be8f46fb5b29631002a2f1343e0cbeae54
Ryan Luong <rluong@tae.com>
2025-04-29 17:55:40 -0700

    Add msdbcsv to Aliases

--- my_custom_aliases.ps1
+++ my_custom_aliases.ps1
@@ -1,3 +1,4 @@
 # New-Alias cdmachinestatedatabase Set-Location .\Documents\Projects\Git\machine_state_database
 
-function cdmachinestatedatabase { set-location "$PROFILE\Documents\Projects\Git\machine_state_database" }
+function cdmachinestatedatabase { set-location "$HOME\Documents\Projects\Git\machine_state_database" }
+function cdmsdbcsv { set-location "$HOME\Documents\Projects\Git\tae\msdb_csv" }
