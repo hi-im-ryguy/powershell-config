ef47ce51d703259ff1b8a6320cec0c93abcb76dc
Ryan Luong <rluong@tae.com>
2025-05-22 18:07:08 -0700

    Add "restart_window_switcher" function
    Useful when window-switcher gets soft-locked by some unknown bug.

--- Modules/GeneralCmdlets/GeneralCmdlets.psm1
+++ Modules/GeneralCmdlets/GeneralCmdlets.psm1
@@ -12,3 +12,10 @@ function git_stash_but_keep {
 function delete_shada_file {
     rm ~/AppData/Local/nvim-data/shada
 }
+
+function restart_window_switcher {
+    $id = (Get-Process window-switcher)[0].Id
+    $path_to_window_switcher = "~\AppData\Local\Programs\window-switcher\window-switcher.exe"
+    Stop-Process -Id $id
+    Start-Process $path_to_window_switcher
+}
